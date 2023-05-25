f6348dde4efd60f70ea84f57e8f95d9effafad91
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-05-26 01:58:47 +0200

    Exercise complete

--- script.js
+++ script.js
@@ -13,18 +13,19 @@ console.log(getDate());
 
 
 // ending of countdown
-const endOfTime = new Date("May 26, 2023 09:30:00")
+const endOfTime = new Date("May 26, 2023 09:30:00");
 //literally now
 let now = new Date();
 console.log(now);
 
 //time span between now and ending
 let totalTime = endOfTime - now;
+timeCalc();
 
 //make time proceed
 function timePassing(){
-    totalTime--;
-
+    totalTime = totalTime - 1000;
+    timeCalc();
 }
 
 //remaining time until the end
@@ -32,27 +33,30 @@ const remainingTime = setInterval(timePassing, 1000);
 console.log(remainingTime);
 
 //When everything stops
-setTimeout(clearInterval, totalTime, remainingTime);
+setTimeout(function(){
+        clearInterval(remainingTime);
+        alert ("You died!");
+        }, totalTime + 100);
+    
 
-//calculation from ms
-let seconds = Math.floor(totalTime / 1000);
-let minutes = Math.floor(seconds / 60);
-let hours = Math.floor(minutes / 60);
+function timeCalc(){
 
-console.log(hours)
-
-seconds = seconds % 60;
-minutes = minutes % 60;
-hours = hours % 24;
-
-//all the time class to variable
-let secondCounter = document.querySelector(".seconds-counter");
-secondCounter.innerHTML = seconds;
-
-let minuteCounter = document.querySelector(".minutes-counter");
-minuteCounter.innerHTML = minutes;
-
-let hourCounter = document.querySelector(".hours-counter");
-hourCounter.innerHTML = hours;
-
-
+    //calculation from ms
+    let seconds = Math.floor(totalTime / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+    
+    seconds = seconds % 60;
+    minutes = minutes % 60;
+    hours = hours % 24;
+    
+    //all the time class to variable
+    let secondCounter = document.querySelector(".seconds-counter");
+    secondCounter.innerHTML = seconds.toString().padStart(2,0);
+    
+    let minuteCounter = document.querySelector(".minutes-counter");
+    minuteCounter.innerHTML = minutes.toString().padStart(2,0);
+    
+    let hourCounter = document.querySelector(".hours-counter");
+    hourCounter.innerHTML = hours.toString().padStart(2,0);
+}
