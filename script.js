bd66d8198f88ea2cb1bc73a0b4021aa56f116e18
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-05-25 22:45:59 +0200

    Correct (almost)

--- script.js
+++ script.js
@@ -12,40 +12,46 @@ console.log(getDate());
 */
 
 
-
+// ending of countdown
 const endOfTime = new Date("May 26, 2023 09:30:00")
+//literally now
 let now = new Date();
 console.log(now);
 
+//time span between now and ending
 let totalTime = endOfTime - now;
-console.log(totalTime);
 
-let second = 1000;
-let minute = second * 60;
-let hour = minute * 60;
-let day = hour *24;
-
-
-
+//remaining time until the end
 const remainingTime = setInterval(timePassing, 1000);
+console.log(remainingTime);
 
+//make time proceed
 function timePassing(){
     totalTime--;
 }
 
+//When everything stops
+setTimeout(clearInterval, totalTime, remainingTime);
 
+//calculation from ms
+let seconds = Math.floor(totalTime / 1000);
+let minutes = Math.floor(seconds / 60);
+let hours = Math.floor(minutes / 60);
 
+console.log(hours)
 
+seconds = seconds % 60;
+minutes = minutes % 60;
+hours = hours % 24;
 
+//all the time class to variable
+let secondCounter = document.querySelector(".seconds-counter");
+secondCounter.innerHTML = seconds;
 
+let minuteCounter = document.querySelector(".minutes-counter");
+minuteCounter.innerHTML = minutes;
 
-
-
-
-
-
-
-
-
+let hourCounter = document.querySelector(".hours-counter");
+hourCounter.innerHTML = hours;
 
 
