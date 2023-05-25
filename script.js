c012f2ba2885094be3a632736c06dff1642cf50f
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-05-25 18:02:10 +0200

    New set

--- script.js
+++ script.js
@@ -13,9 +13,19 @@ console.log(getDate());
 
 
 
-const endOfTime = new Date("May 25, 2023 09:30:00")
+const endOfTime = new Date("May 26, 2023 09:30:00")
+let now = new Date();
+console.log(now);
 
-let totalTime;
+let totalTime = endOfTime - now;
+console.log(totalTime);
+
+let second = 1000;
+let minute = second * 60;
+let hour = minute * 60;
+let day = hour *24;
+
+
 
 const remainingTime = setInterval(timePassing, 1000);
 
